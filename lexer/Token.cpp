bff7d160971de1891eb3dc2de898b0032f4a9d33
Justin <84982572+JustINCodingUK@users.noreply.github.com>
2026-03-07 00:47:36 +0530

    Create header and impl for tokens

---
+++ lexer/Token.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <variant>
+#include "Token.h"
+
+std::string Token::toString() {
+    if (std::holds_alternative<std::string>(value)) {
+        return std::get<std::string>(value);
+    }
+    return "";
+}
+
+Token Token::monostate(TokenType type, int line, int column) {
+    return {type, std::monostate(), line, column};
+}
---
+++ lexer/Token.h
@@ -0,0 +1,33 @@
+#ifndef PYC___TOKEN_H
+#define PYC___TOKEN_H
+#include <string>
+#include <variant>
+
+#endif //PYC___TOKEN_H
+
+enum class TokenType {
+    // Symbols
+    LParen, RParen, LBracket, RBracket, LBrace, RBrace, Comma, Dot, Semicolon, Colon, Arrow, Newline, EndOfFile,
+    // Binary Operator
+    Plus, Minus, Multiply, Divide, Modulo, Power, FloorDivide, Assign, Equal, NotEqual, LessThan,
+    GreaterThan, PlusAssign, MinusAssign, MultiplyAssign, DivideAssign, ModuloAssign, PowerAssign,
+    FloorDivideAssign, LessThanOrEqual, GreaterThanOrEqual, DoubleAmpersand, DoublePipe,
+    // Unary Operator
+    Increment, Decrement, UnaryMinus, UnaryPlus,
+    // Keywords
+    If, Else, Elif, While, For, Def, Class, Return, Ext, Import, As, Try, Catch, Finally, With,
+    Break, Continue, And, Or, Not, In, Is, True, False, Nonlocal, Del, Assert, Async, Await,
+    // Constants
+    Identifier, Literal
+
+};
+
+struct Token {
+    TokenType type;
+    std::variant<std::monostate, std::string, double> value;
+    int line, column;
+
+    std::string toString();
+
+    static Token monostate(TokenType type, int line, int column);
+};
