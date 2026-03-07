92c628d735ac33f26c933d19656e9d1dd3102251
Justin <84982572+JustINCodingUK@users.noreply.github.com>
2026-03-08 02:51:39 +0530

    Add file reading utility

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <fstream>
+
+#include "lexer/Lexer.h"
+#include "parser/Parser.h"
 
 int main(int argc, char** argv) {
     if (argc<2) {
         std::cerr << "No input files";
         return 1;
+    }
+
+    Lexer lexer;
+    for (int i=1;i<argc;++i) {
+        if (std::ifstream file(argv[i]); file.is_open()) {
+            std::string contents((std::istreambuf_iterator(file)), std::istreambuf_iterator<char>());
+            auto tokens = lexer.tokenize(contents);
+        }
     }
 }
