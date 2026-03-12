58ffa708862cb97856892599c31af9c8923adb74
Justin <84982572+JustINCodingUK@users.noreply.github.com>
2026-03-13 01:30:03 +0530

    Implement symbol class and a symbol table

---
+++ analyzer/Symbol.cpp
@@ -0,0 +1,5 @@
+//
+// Created by justinw on 12/03/2026.
+//
+
+#include "Symbol.h"
---
+++ analyzer/Symbol.h
@@ -0,0 +1,37 @@
+//
+// Created by justinw on 12/03/2026.
+//
+
+#ifndef TYPHON_SYMBOL_H
+#define TYPHON_SYMBOL_H
+#include <memory>
+#include <stack>
+#include <unordered_map>
+
+#include "Type.h"
+#include "../parser/Common.h"
+
+struct Symbol {
+    std::string name;
+    const Type type;
+    Visibility visibility;
+    bool isInitialized;
+
+    Symbol(std::string name, Type type, const Visibility visibility, const bool isInitialized)
+        : name(std::move(name)), type(std::move(type)), visibility(visibility), isInitialized(isInitialized) {}
+};
+
+class SymbolTable {
+    std::stack<std::unordered_map<std::string, Symbol>> scopes;
+public:
+    SymbolTable();
+    ~SymbolTable();
+
+    void enterScope();
+    void exitScope();
+    void define(const std::string &name, Symbol sym);
+    std::unique_ptr<Symbol> resolve(const std::string &name);
+};
+
+
+#endif //TYPHON_SYMBOL_H
